21cbca8c34f6d6d7168e4f12c90363c627191910
yangliusi0826 <18473481918@163.com>
2020-03-07 21:45:51 +0800

    安装依赖，封装路由

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from './plugins/axios'
+import router from './router'
 
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
