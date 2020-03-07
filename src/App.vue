28f1fbce80b36fa83c8f61702c9bdc65a02c203b
yangliusi0826 <18473481918@163.com>
2020-03-07 21:04:11 +0800

    设置目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-// import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  mounted () {
-    let url = '/5e5fa191dc856429bc70dee9/example/query'
-    this.$axios.get(url).then((resp) => {
-      console.log('11111111111111')
-      console.log(resp)
-    })
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
